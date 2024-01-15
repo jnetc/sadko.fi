b544f5969c0812e063e333430b6ac2bfdd2b2001
Anton <frontendevs@gmail.com>
2024-01-15 20:26:36 +0200

    netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,25 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.sadko.fi",
-  server: { host: true },
+  server: {
+    host: true
+  },
   compressHTML: true,
   output: 'server',
-  adapter: vercel({ functionPerRoute: true }),
+  adapter: netlify(),
   build: {
-    inlineStylesheets: `always`,
+    inlineStylesheets: `always`
   },
   i18n: {
     defaultLocale: 'ru',
     locales: ['ru', 'fi'],
     routing: {
-      prefixDefaultLocale: true,
-    },
-  },
+      prefixDefaultLocale: true
+    }
+  }
 });
