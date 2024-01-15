bd6a89f386f0e0feccbd3cf010e43bb10b137f8f
Anton <frontendevs@gmail.com>
2024-01-15 19:50:22 +0200

    commit cms files

--- src/lib/localdata.ts
+++ src/lib/localdata.ts
@@ -1,30 +1,30 @@
-import fs from "node:fs"; // 👈 "node:" prefix is an Astro requirement for Node libs
-import FetchFromDatoCMS from "./datocms";
-import type { IData, TypeLanguages } from "@Types";
+// import fs from "node:fs"; // 👈 "node:" prefix is an Astro requirement for Node libs
+// import FetchFromDatoCMS from "./datocms";
+// import type { IData, TypeLanguages } from "@Types";
 
-const CACHE_FOLDER = "./.cache";
-const JSON_FILE = "./.cache/local.json";
+// const CACHE_FOLDER = "./.cache";
+// const JSON_FILE = "./.cache/local.json";
 
-export default async function LocaleData(lang: TypeLanguages) {
-  let data;
+// export default async function LocaleData(lang: TypeLanguages) {
+//   let data;
 
-  if (!fs.existsSync(CACHE_FOLDER)) {
-    fs.mkdirSync(CACHE_FOLDER, { recursive: true });
-  }
+//   if (!fs.existsSync(CACHE_FOLDER)) {
+//     fs.mkdirSync(CACHE_FOLDER, { recursive: true });
+//   }
 
-  if (fs.existsSync(JSON_FILE)) {
-    console.info("__________ LOCAL DATA ___________");
-    // Read data from file
-    const raw = fs.readFileSync(JSON_FILE);
+//   if (fs.existsSync(JSON_FILE)) {
+//     console.info("__________ LOCAL DATA ___________");
+//     // Read data from file
+//     const raw = fs.readFileSync(JSON_FILE);
 
-    data = JSON.parse(raw as unknown as string);
-    return data as IData;
-  } else {
-    console.info("__________ DATA FROM CMS ___________");
+//     data = JSON.parse(raw as unknown as string);
+//     return data as IData;
+//   } else {
+//     console.info("__________ DATA FROM CMS ___________");
 
-    data = await FetchFromDatoCMS(lang);
-    // Write projects to "caching" file
-    fs.writeFileSync(JSON_FILE, JSON.stringify(data));
-    return data;
-  }
-}
+//     data = await FetchFromDatoCMS(lang);
+//     // Write projects to "caching" file
+//     fs.writeFileSync(JSON_FILE, JSON.stringify(data));
+//     return data;
+//   }
+// }
